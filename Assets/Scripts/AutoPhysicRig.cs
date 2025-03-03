39178b35ade69543e2904671b34b79558124303b
Piercourt Charles <113095075+Dr-Charlous@users.noreply.github.com>
2025-03-03 17:30:14 +0100

    [DEV] Update variables + more anims + unity retro version

--- Assets/Scripts/AutoPhysicRig.cs
+++ Assets/Scripts/AutoPhysicRig.cs
@@ -5,31 +5,45 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody))]
 public class AutoPhysicRig : MonoBehaviour
 {
+    [Header("End branches :")]
     [SerializeField] Transform[] _endChains;
     [SerializeField] Transform[] _endChainsAnim;
-    [SerializeField] Transform _animModel;
-    [SerializeField] bool _isWolrdRotationEnable = true;
+
+    [Header("Anim scripts config :")]
     [SerializeField] bool _isLocalRotationBonesEnable = true;
     [SerializeField] bool _isOffsetedBonesEnable = true;
 
+    [Header("Joints config :")]
+    [SerializeField] float _positionSpring = 20000f;
+    [SerializeField] float _positionDamper = 1000f;
+    [SerializeField] bool _useAcceleration = true;
+    [SerializeField] bool _configuredInWorldSpace = false;
+
+    [Header("Rigidbodies config :")]
+    [SerializeField] bool _useGravity = true;
+    [SerializeField] RigidbodyInterpolation _interpolation = RigidbodyInterpolation.Interpolate;
+    [SerializeField] CollisionDetectionMode _collisionDetectionMode = CollisionDetectionMode.Continuous;
+
     private void Start()
     {
-        Rigidbody rb = GetComponent<Rigidbody>();
-
-        if (rb != null)
-        {
-            rb.useGravity = false;
-            rb.isKinematic = true;
-            rb.constraints = RigidbodyConstraints.FreezeAll;
-            rb.linearVelocity = Vector3.zero;    //linearVelocity because unity 6 else velocity
-            rb.angularVelocity = Vector3.zero;
-        }
+        //Initialize();
 
         for (int i = 0; i < _endChains.Length; i++)
         {
             Physicate(_endChains[i], _endChainsAnim[i]);
         }
     }
+
+    void Initialize() //Set-up rigidbody trunk model
+    {
+        Rigidbody rb = GetComponent<Rigidbody>();
+
+        rb.useGravity = false;
+        rb.isKinematic = true;
+        rb.constraints = RigidbodyConstraints.FreezeAll;
+        rb.velocity = Vector3.zero;    //linearVelocity because unity 6 else velocity
+        rb.angularVelocity = Vector3.zero;
+    } 
 
     Rigidbody Physicate(Transform tf, Transform tfAnim)
     {
@@ -47,16 +61,15 @@ public class AutoPhysicRig : MonoBehaviour
 
         //Joint config
         JointDrive drive = new JointDrive();  //Setup joint drive for motion respond
-        drive.positionSpring = 20000f;
-        drive.positionDamper = 1000f;
+        drive.positionSpring = _positionSpring;
+        drive.positionDamper = _positionDamper;
         drive.maximumForce = Mathf.Infinity;
-        drive.useAcceleration = true;
+        drive.useAcceleration = _useAcceleration;
 
         ph.angularXDrive = drive;   //Apply motion parameter on axis
         ph.angularYZDrive = drive;
         ph.rotationDriveMode = RotationDriveMode.XYAndZ;  //Rotation mod
-        if (_isWolrdRotationEnable)
-            ph.configuredInWorldSpace = true;    //Can recieve worldspace values
+        ph.configuredInWorldSpace = _configuredInWorldSpace;    //Can recieve worldspace values
 
         //Liberty of movements for articulations
         ph.angularXMotion = ConfigurableJointMotion.Free;
