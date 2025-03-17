7892c8d00966addce62c8ea7835c2969d6665709
Piercourt Charles <113095075+Dr-Charlous@users.noreply.github.com>
2025-03-17 14:14:21 +0100

    [DEV] Update play mode joints values + corrections in values

--- Assets/Scripts/AutoPhysicRig.cs
+++ Assets/Scripts/AutoPhysicRig.cs
@@ -1,10 +1,12 @@
-using System.Drawing;
+using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody))]
 public class AutoPhysicRig : MonoBehaviour
 {
+    [Tooltip("Actualize joints values on model in play mode")] public bool Actualize = false;
+
     [Header("End branches :")]
     [SerializeField] Transform[] _endChains;
     [SerializeField] Transform[] _endChainsAnim;
@@ -16,17 +18,20 @@ public class AutoPhysicRig : MonoBehaviour
     [Header("Joints config :")]
     [SerializeField] float _positionSpring = 20000f;
     [SerializeField] float _positionDamper = 1000f;
+    [SerializeField][Range(0f, 1f)] float _connectedMassScale = 0.3f; //Scale spring short/long
     [SerializeField] bool _useAcceleration = true;
-    [SerializeField] bool _configuredInWorldSpace = false;
+    [SerializeField] bool _configuredInWorldSpace = false; //Get world position/rotation
 
     [Header("Rigidbodies config :")]
     [SerializeField] bool _useGravity = true;
     [SerializeField] RigidbodyInterpolation _interpolation = RigidbodyInterpolation.Interpolate;
     [SerializeField] CollisionDetectionMode _collisionDetectionMode = CollisionDetectionMode.Continuous;
 
+    List<ConfigurableJoint> _joints = new List<ConfigurableJoint>();
+
     private void Start()
     {
-        //Initialize();
+        Initialize(); //If Rigidbody not set correctly
 
         for (int i = 0; i < _endChains.Length; i++)
         {
@@ -34,15 +39,28 @@ public class AutoPhysicRig : MonoBehaviour
         }
     }
 
-    void Initialize() //Set-up rigidbody trunk model
+    private void Update()
+    {
+        if (Actualize)
+        {
+            for (int i = 0; i < _joints.Count; i++)
+            {
+                JointSetUp(_joints[i]);
+            }
+
+            Actualize = false;
+        }
+    }
+
+    void Initialize() //Root set-up rigidbody
     {
         Rigidbody rb = GetComponent<Rigidbody>();
 
         rb.useGravity = false;
         rb.isKinematic = true;
         rb.constraints = RigidbodyConstraints.FreezeAll;
-        rb.velocity = Vector3.zero;    //linearVelocity because unity 6 else velocity
-        rb.angularVelocity = Vector3.zero;
+        //rb.velocity = Vector3.zero;    //"linearVelocity" because of unity 6 else "velocity"
+        //rb.angularVelocity = Vector3.zero;
     } 
 
     Rigidbody Physicate(Transform tf, Transform tfAnim)
@@ -55,34 +73,17 @@ public class AutoPhysicRig : MonoBehaviour
             return rb;
         }
 
-        var ph = tf.AddComponent<ConfigurableJoint>();
+        var joint = tf.AddComponent<ConfigurableJoint>();
         var anim = tf.AddComponent<AnimatePhysicJoint>();
         rb = tf.GetComponent<Rigidbody>();
+        _joints.Add(joint);
 
-        //Joint config
-        JointDrive drive = new JointDrive();  //Setup joint drive for motion respond
-        drive.positionSpring = _positionSpring;
-        drive.positionDamper = _positionDamper;
-        drive.maximumForce = Mathf.Infinity;
-        drive.useAcceleration = _useAcceleration;
-
-        ph.angularXDrive = drive;   //Apply motion parameter on axis
-        ph.angularYZDrive = drive;
-        ph.rotationDriveMode = RotationDriveMode.XYAndZ;  //Rotation mod
-        ph.configuredInWorldSpace = _configuredInWorldSpace;    //Can recieve worldspace values
-
-        //Liberty of movements for articulations
-        ph.angularXMotion = ConfigurableJointMotion.Free;
-        ph.angularYMotion = ConfigurableJointMotion.Free;
-        ph.angularZMotion = ConfigurableJointMotion.Free;
-        ph.xMotion = ConfigurableJointMotion.Locked;
-        ph.yMotion = ConfigurableJointMotion.Locked;
-        ph.zMotion = ConfigurableJointMotion.Locked;
+        JointSetUp(joint); //Joint
 
         //Rigidbody
-        rb.useGravity = true;
-        rb.interpolation = RigidbodyInterpolation.Interpolate;
-        rb.collisionDetectionMode = CollisionDetectionMode.Continuous;
+        rb.useGravity = _useGravity;
+        rb.interpolation = _interpolation;
+        rb.collisionDetectionMode = _collisionDetectionMode;
 
         //AnimeScript
         anim.TargetBone = tfAnim;
@@ -90,10 +91,34 @@ public class AutoPhysicRig : MonoBehaviour
         anim.IsOffseted = _isOffsetedBonesEnable;
 
         if (tf.parent.GetComponent<AnimatePhysicJoint>() != null || tf.parent.GetComponent<AutoPhysicRig>() != null)
-            ph.connectedBody = tf.parent.GetComponent<Rigidbody>();
+            joint.connectedBody = tf.parent.GetComponent<Rigidbody>();
         else
-            ph.connectedBody = Physicate(tf.parent, tfAnim.parent);
+            joint.connectedBody = Physicate(tf.parent, tfAnim.parent);
 
         return rb;
+    }
+
+    void JointSetUp(ConfigurableJoint joint)
+    {
+        //Joint config
+        JointDrive drive = new JointDrive();  //Setup joint drive for motion respond
+        drive.positionSpring = _positionSpring;
+        drive.positionDamper = _positionDamper;
+        drive.maximumForce = Mathf.Infinity;
+        drive.useAcceleration = _useAcceleration;
+
+        joint.angularXDrive = drive;   //Apply motion parameter on axis
+        joint.angularYZDrive = drive;
+        joint.rotationDriveMode = RotationDriveMode.XYAndZ;  //Rotation mod
+        joint.configuredInWorldSpace = _configuredInWorldSpace;    //Can recieve worldspace values
+        joint.connectedMassScale = _connectedMassScale;
+
+        //Liberty of movements for articulations
+        joint.angularXMotion = ConfigurableJointMotion.Free;
+        joint.angularYMotion = ConfigurableJointMotion.Free;
+        joint.angularZMotion = ConfigurableJointMotion.Free;
+        joint.xMotion = ConfigurableJointMotion.Locked;
+        joint.yMotion = ConfigurableJointMotion.Locked;
+        joint.zMotion = ConfigurableJointMotion.Locked;
     }
 }
